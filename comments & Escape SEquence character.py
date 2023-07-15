0cce8bb9db7af349d5c161a9c0c8560e540d1b2a
UngraduateGamer <139570725+UngraduateGamer@users.noreply.github.com>
2023-07-15 13:31:29 +0530

    Update comments & Escape Sequence character.py

--- comments & Escape SEquence character.py
+++ comments & Escape SEquence character.py
@@ -8,3 +8,10 @@ say hello
 # and \t used for print the tab space
 
 print("hey this is first line\nHey this is Second Line")
+
+'''parameter of print statements: ends and sep
+      print("I am the","best","no one can beat me",sep="-",ends="")'''
+
+'''now you can print the double quotes in the double quotes strings so we can use backslash eg.
+eg. print("Hello \"Brother\" ")
+'''
